5c539af7d9b2a6e3b615300ce3556d1e34b0cf46
Michal Žamboch <zamboch.michal@seznam.cz>
2023-08-27 15:48:36 +0200

    Trie algorithms and test.

--- trie/src/main.rs
+++ trie/src/main.rs
@@ -1,7 +1,21 @@
-use trie::{TrieNode};
-
 mod trie;
+use trie::*;
 
 fn main() {
-    
+    let mut trie_test = TrieStruct::create();
+
+    trie_test.insert("Test".to_string());
+    trie_test.insert("Tea".to_string());
+    trie_test.insert("Background".to_string());
+    trie_test.insert("Back".to_string());
+    trie_test.insert("Brown".to_string());
+
+    println!(
+        "Is Testing in the trie? {}",
+        trie_test.find("Testing".to_string())
+    );
+    println!(
+        "Is Brown in the trie? {}",
+        trie_test.find("Brown".to_string())
+    );
 }
--- trie/src/trie.rs
+++ trie/src/trie.rs
@@ -1,15 +1,7 @@
 
 use std::collections::HashMap;
 
-/*
-pub trait ITrieNode {
-    fn new(c: char, is_final: bool) -> TrieNode;
-    fn new_root() -> TrieNode;
-    fn check_value(self, c: char) -> bool;
-    fn insert_value(&mut self, c: char, is_final: bool);
-}
-*/
-
+#[derive(Debug)]
 pub struct TrieNode {
     value: Option<char>,
     is_final: bool,
@@ -39,5 +31,75 @@ impl TrieNode {
 
     fn insert_value(&mut self, c: char, is_final: bool) {
         self.child_nodes.insert(c, TrieNode::new(c, is_final));
+    }
+}
+
+#[derive(Debug)]
+pub struct TrieStruct {
+    root_node: TrieNode,
+}
+
+impl TrieStruct {
+    pub fn create() -> TrieStruct {
+        TrieStruct {
+            root_node: TrieNode::new_root(),
+        }
+    }
+
+    pub fn insert(&mut self, string_val: String) {
+        let mut current_node = &mut self.root_node;
+        let char_list: Vec<char> = string_val.chars().collect();
+        let mut last_match = 0;
+
+        for letter_counter in 0..char_list.len() {
+            if current_node
+                .child_nodes
+                .contains_key(&char_list[letter_counter])
+            {
+                current_node = current_node
+                    .child_nodes
+                    .get_mut(&char_list[letter_counter])
+                    .unwrap();
+            } else {
+                last_match = letter_counter;
+                break;
+            }
+            last_match = letter_counter + 1;
+        }
+
+        if last_match == char_list.len() {
+            current_node.is_final = true;
+        } else {
+            for new_counter in last_match..char_list.len() {
+                println!(
+                    "Inserting {} into {}",
+                    char_list[new_counter],
+                    current_node.value.unwrap_or_default()
+                );
+                current_node.insert_value(char_list[new_counter], false);
+                current_node = current_node
+                    .child_nodes
+                    .get_mut(&char_list[new_counter])
+                    .unwrap();
+            }
+            current_node.is_final = true;
+        }
+    }
+
+    pub fn find(&mut self, string_val: String) -> bool {
+        let mut current_node = &mut self.root_node;
+        let char_list: Vec<char> = string_val.chars().collect();
+
+        for counter in 0..char_list.len() {
+            if !current_node.child_nodes.contains_key(&char_list[counter]) {
+                return false;
+            } else {
+                current_node = current_node
+                    .child_nodes
+                    .get_mut(&char_list[counter])
+                    .unwrap();
+            }
+        }
+        return true;
     }
 }
